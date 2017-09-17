45540797f8128cb1a58d9131866cc67b923db809
xyxzjhjy <31675186+xyxzjhjy@users.noreply.github.com>
2017-09-17 13:12:15 -0400

    Update citibike.sh
    Make clear

--- citibike.sh
+++ citibike.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir citibke_allc
-cd  citibke_allc
+mkdir ../citibike_all
+cd  ../citibike_allc
 for i in $(seq 9) ; do
   wget https://witestlab.poly.edu/bikes/20160"$i"-citibike-tripdata.zip
 done
@@ -9,10 +9,12 @@ for i in $(seq 9) ; do
   awk -F "\"*,\"*" '{print $5}' 20160"$i"-citibike-tripdata.csv >> station.txt
 done
 #awk -F "\"*,\"*" '{print $5}' 201601-citibike-tripdata.csv >> station.txt
-cat station.txt | sort>sort.txt
+#cat station.txt | sort>sort.txt
 #cat station.txt | sort -u>uniq.txt
 #wc -l uniq.txt
-sort sort.txt | uniq -dc > num.txt
-sort -n  num.txt>ans.txt
-tail --lines=1 ans.txt
+#sort sort.txt | uniq -dc > num.txt
+#sort -n  num.txt>ans.txt
+#tail --lines=1 ans.txt
 #wc -l ans.txt
+cat station.txt | sort | uniq -dc | sort -n | tail --lines=1 >>answer.txt
+cat answer.txt
